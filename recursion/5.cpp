871ff5d90aa8c4c1cf1b378e183633ae27803af5
milan Katira <milankatira26@gmail.com>
2022-01-25 23:11:01 +0530

    initial commmit

---
+++ recursion/5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int Power(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a;
+
+    int ans = Power(a, b / 2);
+
+    if (b % 2 == 0)
+    {
+        return ans * ans;
+    }
+    else
+    {
+        return a * ans * ans;
+    }
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int ans = Power(a, b);
+
+    cout << "Answer is " << ans << endl;
+}
---
+++ recursion/bubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void sortArr(int *arr, int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+
+    sortArr(arr, n - 1);
+}
+int main()
+{
+    int arr[5] = {2, 5, 7, 9, 1};
+    sortArr(arr, 5);
+
+    for (int i = 0; i <= 5; i++)
+
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
