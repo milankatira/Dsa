3daf35b419ad2df78af96b96e924aa01fd1d6c27
milan Katira <milankatira26@gmail.com>
2022-01-26 09:48:12 +0530

    initial commit

--- dsaOne/array/5.cpp
+++ dsaOne/array/5.cpp
@@ -5,14 +5,29 @@ int main()
 {
     int n;
     cin >> n;
-    int a[n];
+    int a[n + 1];
+    a[n] = -1;
 
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    cout
-        << "max subarray length is " << ans << " ";
+    if (n == 1)
+    {
+        cout << "1" << endl;
+        return 0;
+    }
+    int ans = 0;
+    int mx = -1;
 
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > mx && a[i] > a[i + 1])
+        {
+            ans++;
+        }
+        mx = max(mx, a[i]);
+    }
+    cout<<"record bracking day " << ans << endl;
     return 0;
 }
