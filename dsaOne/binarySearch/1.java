bc0b49a09c706f59f6614dd428065c657615b499
milan Katira <milankatira26@gmail.com>
2022-02-06 22:34:49 +0530

    initial commit

---
+++ dsaOne/binarySearch/1.java
@@ -0,0 +1,19 @@
+package com.milan;
+
+public class BinarySearch{
+    public static void main(String[] args){
+
+    int arr[]={-18,-12,-4,0,2,3,4,15,16,18,22,45,89};
+    int target=22;
+
+    int ans=binarySearch(arr,target);
+    System.out.println(ans);
+    }
+
+    static int binarySearch(int [] arr, int target){
+        int start=0;
+        int end=arr.length-1;
+    }
+
+}
+
